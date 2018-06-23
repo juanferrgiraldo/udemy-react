59f9a3a0d01e981b32465b1941789cd8ad2781f5
Juan Fernando <juanferrgiraldo@gmail.com>
2018-06-23 18:45:28 -0500

    M4: Rendering content conditionally, not optimal way.

---
+++ udemy-app/src/App.js
@@ -0,0 +1,101 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+  state = {
+    persons: [
+      {name: "Juan", age: "21"},      
+      {name: "Daniela", age: "23"},
+      {name: "Andrea", age: "21"}
+    ],
+    otherState: [
+      {name: "This is another state"}
+    ],
+    showPersons: false
+  }
+
+  switchNameHandler = (newName) => {
+    // DON'T DO  THIS: this.state.persons[0].name = 'Juanfis';
+    this.setState({
+      persons: [
+        {name: newName, age: "21"},
+        {name: "Daniela", age: "23"},
+        {name: "Andrea", age: "21"}        
+      ]
+    })
+  }
+
+  nameChangeHandler = (event) => {
+    this.setState({
+      persons: [
+        {name: "Juan", age: "21"},
+        {name: event.target.value, age: "23"},
+        {name: "Andrea", age: "21"}        
+      ]
+    })
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({
+      showPersons: !doesShow
+    })
+  }
+
+  render() {
+    // Inline style
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'    
+    }
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to my React App</h1>          
+        </header>
+        { 
+          this.state.showPersons === true ?
+          <div>
+            <Person 
+              name={this.state.persons[0].name}
+              age={this.state.persons[0].age}
+              click={this.switchNameHandler.bind(this, 'Juanfis')}> {/*Best way to pass methods by reference*/}
+              Hobbies: Code and skateboarding.
+            </Person>
+            <Person 
+              name={this.state.persons[1].name} 
+              age={this.state.persons[1].age}
+              change={this.nameChangeHandler}>
+              Hobbies: Read and draw.
+            </Person>
+            <Person 
+              name={this.state.persons[2].name} 
+              age={this.state.persons[2].age}>
+              Hobbies: Read and draw.
+            </Person>
+            {/*<Person name={this.state.otherState[0].name} /> --> not a properly way to make it*/}
+          </div> : null 
+        }                 
+        <button 
+          onClick={() => this.switchNameHandler('Juanfis!')}
+          style={style}>
+          Switch Name
+        </button> {/*Second way to pass methods by reference, not recommended, inefficient*/}
+        <button 
+          onClick={this.togglePersonsHandler}
+          style={style}>
+          Toggle Persons
+        </button>
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, "Welcome to my React App"));
+  }
+}
+
+export default App;
---
+++ udemy-app/src/Person/Person.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Person.css';
+
+const person = (props) => {
+    return (
+        <div className="Person">
+            <h4 onClick={props.click}>I'm {props.name} and I'm {props.age} years old.</h4>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.change} value={props.name} /> {/*Two way binding example*/}
+        </div>    
+    )
+};
+
+export default person
