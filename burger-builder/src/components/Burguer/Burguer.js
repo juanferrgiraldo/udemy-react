b5719474360ad2fa81106a081bf14475f47058b9
Juan Fernando <juanferrgiraldo@gmail.com>
2018-08-03 12:25:45 -0500

    BB: Advance in build control and burguer price.

---
+++ burger-builder/src/components/Burguer/Burguer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import classes from './Burguer.css';
+import BurguerIngredient from './BurguerIngredient/BurguerIngredient';
+
+const burguer = (props) => {
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {            
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurguerIngredient key={igKey + i} type={igKey} />
+            });            
+        }).reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);    
+        if (transformedIngredients.length === 0)   {
+            transformedIngredients = <p>Please start adding ingredients!</p>
+        }
+    return (
+        <div className={classes.Burguer}>
+            <BurguerIngredient type="bread-top" />
+            {transformedIngredients}
+            <BurguerIngredient type="bread-bottom" />
+        </div>
+    );
+}
+
+export default burguer;
